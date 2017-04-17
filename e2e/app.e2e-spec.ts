fe31b0d634a6e9a3c38655dae943033fe44cb328
Guilherme Oliveira <gui.shift@gmail.com>
2017-04-17 00:53:02 -0300

    Initial tests e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,28 @@
 import { AngularTourOfHeroesPage } from './app.po';
+import { browser, element, by, ElementFinder, ElementArrayFinder } from 'protractor';
 
 describe('angular-tour-of-heroes App', function() {
-  let page: AngularTourOfHeroesPage;
 
   beforeEach(() => {
-    page = new AngularTourOfHeroesPage();
+    browser.get('/dashboard');
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should display title app "Tour of Heroes"', () => {
+    element(by.tagName('h1')).getText()
+    .then(text => expect(text === 'Tour of Heroes'));
+  });
+
+  fit('should be clickable link "Heroes"', () => {
+    let heroes = element.all(by.tagName('a')).get(1);
+    heroes.click();
+
+    let li = element.all(by.tagName('li')).get(2);
+    li.click();
+
+    let button: ElementFinder = element(by.tagName('button') && by.buttonText('View Details'));
+    
+    button.click();
+    browser.pause();
+    expect(element(by.tagName('button') && by.buttonText('Save'))).toBeTruthy();
   });
 });
